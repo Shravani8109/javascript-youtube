bc58b7b0984aea0fcaeaf077fec62a18d573bbad
Shravani Patil <167869874+Shravani8109@users.noreply.github.com>
2025-05-01 09:21:40 +0000

    datatype conversion

---
+++ datatypes.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// alert(3+3)  we are using nodejs, not browser
+
+// let score = 22
+// let score = "22abc"
+// let score = null
+// let score = undefined
+// console.log(typeof score);
+// console.log(typeof(score))
+//Number convert string to number type
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+//"22abc" is not a number it will convert to number but value is NaN
+// console.log(valueInNumber);  
+
+//33 =>33
+//"33abd" => NaN
+//true => 1, false=>0
+
+// convert number to boolean
+// let isLoggedIn = "shravani"
+
+// let isBooleanLoggedIn = Boolean(isLoggedIn)
+// console.log(isBooleanLoggedIn);
+
+// let isLoggedIn = ""            o/p =>false
+// let isLoggedIn = "shravani"     => true
+//1 --- true
+//0 --false
+
+// number to string
+
+let somenumber = 33
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
