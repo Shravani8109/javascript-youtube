852b041480d7d3e7646093908c24bf9ba9db2bc9
Shravani Patil <167869874+Shravani8109@users.noreply.github.com>
2025-05-02 18:32:41 +0000

    Math and num

---
+++ .devcontainer/01_basics/nums_maths.js
@@ -0,0 +1,37 @@
+// const scrore = 400
+// console.log(scrore);
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+//it will give precision after number 
+// console.log(balance.toFixed(3));
+
+// const othernumber = 123.9000
+// console.log(othernumber.toPrecision(4));
+
+//comma add karte ,readable format
+// const hundreds = 10000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++Maths+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+//round--value 4.5 cha var asel tr 5 count karte 4.5 cha khali asel tr 4
+// console.log(Math.round(4.6));
+//ceil 4.6 asel tr 5 karte ,5.6 asel tr 6 karte,mothi value ghete
+// console.log(Math.ceil(4.9));
+//floor small khalchi value ghte i.e 4.4 tr 4,5.7 tr 5
+// console.log(Math.floor(4.4));
+// console.log(Math.min(2,3,4));
+// console.log(Math.max(2,3,4));
+
+//random() gives any random value between 0 to 1 -- 0.9999 or 0.5355353
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
