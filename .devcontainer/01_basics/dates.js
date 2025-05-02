4ad9689e70450b9db3a112320d9315ba697a795f
Shravani Patil <167869874+Shravani8109@users.noreply.github.com>
2025-05-02 19:02:39 +0000

    dates

---
+++ .devcontainer/01_basics/dates.js
@@ -0,0 +1,21 @@
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toJSON());
+
+// let createMyDate = new Date(2023,0,23)
+// console.log(createMyDate.toDateString());
+
+// let myTimestamp = Date.now()
+// console.log(myTimestamp);
+
+//coverts milisecond to second
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
